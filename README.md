5eb44f7519e252e49d5a2d675be5878e3643e29a
mmmosca <marcomichele.mosca@gmail.com>
2023-12-09 20:30:47 +0000

    Upload first SRC and DOC files

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # cpp-cmd-api
-API for handling command line parameters
+C++ API for handling command line parameters
+
+## Installation (Windows)
+1. Install Visual Studio (e.g. Community) choosing the following `single components`:
+    - `.NET SDK`
+    - `.NET 7.0 Runtime`
+    - `MSVC v142 - C++ Build tools`
+    - `MSVC v143 - C++ Build tools`
+    - `C++/CLI for Build Tools v142`
+    - `Windows 10 SDK (10.2.20348.0)`
+    - `CMake C++ Tools for Windows`
+    - `Git for Windows`
+
+2. Download the repository
+
+3. Add the following environmental variable as User:
+    - `VS_DIR`: Path to Visual Studio with all folders (e.g. `Microsoft Visual Studio\2022\Community`)
+
+4. Run the following in a `Command Prompt` (no PowerShell) to install it:
+    ```
+    "%VS_DIR%\VC\Auxiliary\Build\vcvarsall.bat" x64_x86
+    cd cpp-cmd-api
+    cmake -B build -S . -L -DCMAKE_INSTALL_PREFIX=<install_folder> -DSHARED=ON
+    cmake --build build --target install --config Release
+    ```
+5. When using the library in your project make sure to:
+    - Include `<install_folder>/include`
+    - Link the shared library in `<install_folder>/lib`
---
+++ include/cmd-api.h
@@ -0,0 +1,60 @@
+/*
+This program is free software: you can redistribute it and/or 
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/.
+ 
+Author Marco M. Mosca, email: marcomichele.mosca@gmail.com
+*/
+#ifndef CMD_H_
+#define CMD_H_
+
+#ifdef DEBUG
+#define PRINT_DATA()	printf("CURRENT ---- OPTIND: %d ---- ARGVIND: %d ---- OPTION: %c ---- OPTARG: %s\n", optind, argvind, argv[optind][1], myoptarg);
+#define PRINT_DATAW()	printf("CURRENT ---- OPTIND: %d ---- ARGVIND: %d ---- OPTION: %s ---- OPTARG: %s\n", optind, argvind, argv[optind], myoptarg);
+#else
+#define PRINT_DATA()	printf("OPTION: %c ---- OPTARG: %s\n", argv[optind][1], myoptarg);
+#define PRINT_DATAW()	printf("OPTION: %s ---- OPTARG: %s\n", argv[optind], myoptarg);
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+
+int isCharInString(char c, char* str);
+
+int AreStringsEqualFrom(const char* s1, const char* s2, int from);
+
+int isSubstring(char* sub, char* str);
+
+char* strsep(char** elem_pointer, char* pattern);
+
+struct CommandLine {
+private:
+	/*option index in the array of line commands*/
+	/*argument index in the array of line commands*/
+	int optind, argvind, start, formatind, argformatind;
+	/*pointer to the argument*/
+	char *curr_option;
+public:
+	char* myoptarg;
+
+	CommandLine() : start{ 1 }, formatind{ -1 }, argformatind{ 0 } {
+		reset_values();
+	};
+
+	void reset_values();
+
+	char mygetopt(int argc, char** argv, char* format);
+
+	char* mygetoptW(int argc, char** argv, char* format);
+};
+#endif /* CMD_H_ */
